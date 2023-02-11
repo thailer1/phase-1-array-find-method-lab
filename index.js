6c1c4c750fe3e3844b61254c44cbf1fb9cbf6811
Thailer <thailer.palmer@gmail.com>
2023-02-11 18:53:40 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: ""},
+  ]
+
+  let superbowlWin = (record) =>{
+    // console.log(record[0].result)
+    let count = 0
+    record[count]  
+    for(const wins of record){
+        if(record[count].result === 'W')return record[count].year
+        count++ 
+        // wins = [result]
+        // console.log(`This is wins this is a win`, wins)
+    //    if(find(wins))
+    }
+  }
+  console.log(superbowlWin(record))
